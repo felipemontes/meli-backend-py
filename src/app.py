16e0143ebf74e8b7ef696b024679e7edb7111fa5
felipemontes <felipe.londono.montes@gmail.com>
2024-01-28 17:39:50 -0500

    add create conversation method

---
+++ src/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+from bson import json_util
+
+app = Flask(__name__)
+client = MongoClient("mongodb://localhost:27017/")
+db = client["conversations"]
+coll = db.conversations
+
+@app.route('/conversations', methods=['POST'])
+def create_conversation():
+    userid = request.json['userid']
+    messages = request.json['messages']
+    if userid and messages:
+        id = db.conversations.insert_one({'userid': userid, 'messages': messages})
+        response = {
+            'id': str(id),
+            'userid': userid,
+            'messages': messages,
+        }
+        return response
+    else:
+        return jsonify({"error": "missing request params"}, 400)
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
